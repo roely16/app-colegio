f95ad88fe14a3b9d467b5acc16100596f4c66650
Alexander <alexanderovidio@gmail.com>
2021-07-08 21:09:50 -0600

    add inscripciones view

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,16 @@ import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
 import Alumnos from '../views/Alumnos.vue'
 import Permisos from '../views/Permisos'
+import Inscripcion from '../views/Inscripcion.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+	{
+		path: '/inscripcion',
+		name: 'inscripcion',
+		component: Inscripcion  
+	},
 	{
 		path: '/',
 		name: 'login',
---
+++ src/views/Inscripcion.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col>
+                <v-btn>Iniciar Proceso</v-btn>
+                <v-btn>Verificar Proceso</v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
